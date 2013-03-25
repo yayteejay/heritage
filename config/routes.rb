9904ddace3fdeb845b1738133ffc123818776276
TJ Taylor <yayteejay@me.com>
2013-03-25 16:33:43 -0700

    css changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Heritage::Application.routes.draw do
+  get 'about' => 'pages#about'
+  
+
+  root :to => 'pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
